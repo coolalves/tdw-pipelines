5088e895738ac286422aca831aff7498447ff5ad
Daniel Alves <alvesdaniel@ua.pt>
2022-10-27 23:44:31 +0100

    next lint done

--- components/alert.js
+++ components/alert.js
@@ -1,6 +1,7 @@
 import Container from "./container";
 import cn from "classnames";
 import { EXAMPLE_PATH } from "../lib/constants";
+import Link from "next/link";
 
 export default function Alert({ preview }) {
   return (
@@ -12,16 +13,17 @@ export default function Alert({ preview }) {
     >
       <Container>
         <div className="py-2 text-center text-sm">
+          {" "}
           {preview ? (
             <>
               This is page is a preview.{" "}
-              <a
+              <Link
                 href="/api/exit-preview"
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
-                Click here
-              </a>{" "}
-              to exit preview mode.
+                Click here{" "}
+              </Link>{" "}
+              to exit preview mode.{" "}
             </>
           ) : (
             <>
@@ -30,13 +32,13 @@ export default function Alert({ preview }) {
                 href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
                 className="underline hover:text-success duration-200 transition-colors"
               >
-                available on GitHub
+                available on GitHub{" "}
               </a>
-              .
+              .{" "}
             </>
-          )}
-        </div>
-      </Container>
+          )}{" "}
+        </div>{" "}
+      </Container>{" "}
     </div>
   );
 }
