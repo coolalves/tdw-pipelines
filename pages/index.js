9af3587f531994fd3bdc146cd62f19049fc711e8
Daniel Alves <alvesdaniel@ua.pt>
2022-10-31 16:33:26 +0000

    minor aesthetic changes

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,42 @@ export default function Records({ records }) {
   console.log(records);
 
   return (
+  <div className="main"> 
     <div className="container">
+
+      <div className="header">
+        <h1> My 2022 favorite records (so far)</h1>
+      </div>
+
       <div className="record-list">
         {records.map((record) => (
           <RecordCard key={record.sys.id} record={record} />
-        ))}{" "}
+        ))}
       </div>
 
       <style jsx>{`
+        .main{
+          background-color:red;
+        }
         .container {
-          position: absolute;
+          position: relative;
+          margin-top:auto;
+          margin-bottom:auto;
+          text-align:center;
+          height: auto;
+          margin: 0 auto;
+          adding: 10px;
+          
+        }
+        .header{
+          background-color: black;
+          height: 100px;
+          line-height: 100px;
+        }
+        h1 {
+          font-size: 30px;
+          font-weight: bold;
+          color: white;
         }
         .record-list {
           width: 100%;
@@ -40,5 +66,6 @@ export default function Records({ records }) {
         }
       `}</style>
     </div>
+  </div>
   );
 }
