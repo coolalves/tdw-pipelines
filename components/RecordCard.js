4bc8f3b9752af258da0419648972bd6fcb70e3c8
Daniel Alves <alvesdaniel@ua.pt>
2022-10-31 01:24:18 +0000

    contentful images

--- components/RecordCard.js
+++ components/RecordCard.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
+
 export default function RecordCard({ record }) {
   const { title, artist, slug, thumbnail } = record.fields;
 
   return (
     <div className="card">
-      <div className="featured"> {/*thumbnail*/} </div>{" "}
+      <div className="featured">
+        <Image
+          src={"https:" + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>{" "}
       <div className="content">
         <div className="info">
           <h4> {title} </h4> <p> {artist} </p>{" "}
